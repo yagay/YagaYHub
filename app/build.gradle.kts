5a61d0bbbd184ece2ee0ca4c55a15e3b65bf3fe3
Jie <1614094+yagay@users.noreply.github.com>
2026-09-20 06:20:31 +0100

    Bump YagaYHub to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yagay.YagaYHub"
         minSdk = 31
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
 
     buildFeatures {
