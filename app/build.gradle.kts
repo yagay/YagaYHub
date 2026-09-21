e7e250c7fa3cb5a8d9395333089ec07ecc5b72e3
Jie <1614094+yagay@users.noreply.github.com>
2026-09-21 16:33:30 +0100

    Add Custom Tabs for GitHub web login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.13.0")
     implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.browser:browser:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
